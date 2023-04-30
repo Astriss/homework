41923629e2eedc8ff917b3ee6cda19c2a5d8c747
Astriss <lei.san.min12@gmail.com>
2023-04-30 20:00:32 +0300

    Task 1.1 done

---
+++ task1.1.py
@@ -0,0 +1,6 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+songs=my_favorite_songs.split(',')
+print(songs[0])
+print(songs[-1])
+print(songs[1])
+print(songs[-2])
