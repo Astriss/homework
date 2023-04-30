61f0e2afc4f7e44e00614af8bd3c715c4fbada72
Astriss <lei.san.min12@gmail.com>
2023-04-30 20:26:19 +0300

    A) done

---
+++ task 1.2.py
@@ -0,0 +1,18 @@
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+time1 = float(my_favorite_songs[0][1])
+time2 = float(my_favorite_songs[1][1])
+time3 = float(my_favorite_songs[2][1])
+time = time1 + time2 + time3
+
+
+print(f'Three songs last {time} minutes')
