9c8fcd85f45a5691dc15e711be6c33a52c5e8320
Astriss <lei.san.min12@gmail.com>
2023-05-06 23:46:54 +0300

    task 2.1 done

---
+++ 2/2.1.py
@@ -0,0 +1,28 @@
+
+def minimum(arr):
+    min = nums[0]
+    for i in nums:
+        if i < min:
+            min = i
+        else:
+            pass
+    return min 
+
+
+def maximum(arr):
+    max = nums[0]
+    for i in nums:
+        if i > max:
+            max = i
+        else:
+            pass
+    return max
+
+info = input('Enter integers using "," to split: ')
+nums = info.split(',')
+for i in nums:
+    i = int(i)
+
+
+print(f'Minimum: {minimum(nums)}')
+print(f'Maximum: {maximum(nums)}')
